a8b2e2b71ce361746c701e8d751ae5807a1ef12b
babadu14 <134163406+babadu14@users.noreply.github.com>
2023-12-17 23:55:26 +0400

    Update easy_calculator.py

--- easy_calculator.py
+++ easy_calculator.py
@@ -1,6 +1,6 @@
-number1 = float(input("enter first number -> "))
+number1 = int(input("enter first number -> "))
 operator = input("choose an operator (+, -, /, *, %, **, //) ")
-number2 = float(input("enter second number -> "))
+number2 = int(input("enter second number -> "))
 
 if operator == "+":
     print(number1 + number2)
